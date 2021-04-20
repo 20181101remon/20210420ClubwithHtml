01ba3d7f9d57951dca3c88cc84f2bd3fd59f75e8
unknown <mindy80230@gmail.com>
2021-04-20 14:18:36 +0800

    介面上傳

---
+++ js/apply.js
@@ -0,0 +1,19 @@
+Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire({
+  position: 'top',
+  icon: 'success',
+  title: 'Your work has been saved',
+  showConfirmButton: false,
+  timer: 1500
+})
+  }
+})
